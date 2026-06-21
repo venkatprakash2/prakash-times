5830109d14ad0679235d53b8f7784ec6d9193c46
venkatprakash2 <p.shivarman@gmail.com>
2026-06-21 12:57:41 +0530

    use tmdb bigil as default movie

--- src/data/movies.js
+++ src/data/movies.js
@@ -4,7 +4,11 @@ export const seedMovies = [
     title: 'Bigil',
     status: 'Watched',
     rating: 5,
-    poster: 'https://m.media-amazon.com/images/M/MV5BNGI5YjQ2ZmUtZmY5Mi00MmZjLTk2YzEtNDgzMDJmNzcyNmI0XkEyXkFqcGc@._V1_.jpg',
+    poster: 'https://image.tmdb.org/t/p/w500/s4Yi1RbMyyN3hH2hElwvluP2rOG.jpg',
+    tmdbId: '563309',
+    releaseDate: '2019-10-17',
+    overview:
+      'A do-gooder don wants his footballer son to uplift the life of his people by becoming a champion, but fate draws the youngster into a life of violence. Will he be able to fulfill his father’s dream when an opportunity comes his way seven years later?',
     review: 'best movie ever',
   },
 ]
